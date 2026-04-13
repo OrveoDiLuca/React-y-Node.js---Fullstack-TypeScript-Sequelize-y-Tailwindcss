922b754ecd62b839732a0f871ee487ab81c50318
Orveo <orveo1902@gmail.com>
2026-04-13 14:17:30 -0400

    se agrego el componente de ProductForm

--- src/pages/EditProduct.tsx
+++ src/pages/EditProduct.tsx
@@ -2,6 +2,7 @@ import {Link, Form, useActionData, type ActionFunctionArgs, redirect, type Loade
 import ErrorMessage from '../components/ErrorMessage'
 import { updateProduct, getProductById } from '../services/ProductService'
 import type { Product } from '../types'
+import ProductForm from '../components/ProductForm'
 
 export async function loader({params} : LoaderFunctionArgs) {
     if(params.id !== undefined){
@@ -59,35 +60,9 @@ export default function EditProduct() {
             method='POST'  
         >
     
-            <div className="mb-4">
-            <label
-                className="text-gray-800"
-                htmlFor="name"
-            >Nombre Producto:</label>
-            <input 
-                id="name"
-                type="text"
-                className="mt-2 block w-full p-3 bg-gray-50"
-                placeholder="Nombre del Producto"
-                name="name"
-                defaultValue={product.name}
+            <ProductForm
+                product = {product}
             />
-            </div>
-
-            <div className="mb-4">
-            <label
-                className="text-gray-800"
-                htmlFor="price"
-            >Precio:</label>
-            <input 
-                id="price"
-                type="number"
-                className="mt-2 block w-full p-3 bg-gray-50"
-                placeholder="Precio Producto. ej. 200, 300"
-                name="price"
-                defaultValue={product.price}
-            />
-            </div>
 
             <div className="mb-4">
                 <label
@@ -109,7 +84,7 @@ export default function EditProduct() {
             <input
             type="submit"
             className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded hover:bg-indigo-500"
-            value="Editar Producto"
+            value="Guardar Cambios"
             />
         </Form>
         </>
